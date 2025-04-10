31a23871a53c96072ec71fdea1a9d6f735660050
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-04-10 18:30:06 +0300

    #yiugR7&TF updated

--- src/components/courses/CourseModulesComponent.tsx
+++ src/components/courses/CourseModulesComponent.tsx
@@ -2,9 +2,9 @@ import {FC} from "react";
 
 const CourseModulesComponent: FC<{ modules: string[] }> = ({modules}) => {
     return (
-        <ul className={'modules-list'}>
-            {modules.map((mod: string) => (
-                <li className={'text-gray-700 dark:text-gray-400'}>{mod}</li>
+        <ul className={'!pt-2  modules-list'}>
+            {modules.map((mod, index) => (
+                <li key={index} className={'text-gray-700 dark:text-gray-400'}>{mod}</li>
             ))
             }
         </ul>
