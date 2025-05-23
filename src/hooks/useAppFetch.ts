1c9b05aa42107ea9b7b07905aa779575d2625201
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-05-24 01:48:23 +0300

    redux toolkit -

--- src/hooks/useAppFetch.ts
+++ src/hooks/useAppFetch.ts
@@ -1,19 +1,19 @@
 import {useEffect} from "react";
 import {DispatchType, useAppDispatch} from "./useAppDispatch.ts";
 
-const useAppFetch =<T>(serviceFunc: () => Promise<T>, actionFunc: (payload: T) => { type: string, payload: T}): void => {
+const useAppFetch =<T>(serviceFunc: () => Promise<T>, actionFunc: (payload: T) => { type: string, payload: T}) => {
     const dispatch: DispatchType = useAppDispatch()
 
     useEffect(() => {
-        const fetchData = async () => {
+        const fetchApi = async () => {
             const response = await serviceFunc()
             dispatch(actionFunc(response))
         }
 
-        fetchData()
+        fetchApi()
     }, [serviceFunc, actionFunc]);
 
-
+    return
 }
 
 export default useAppFetch
